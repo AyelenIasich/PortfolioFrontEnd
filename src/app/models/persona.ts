c7e9651742d6afd62d5f09fa90744f8d5e17afb3
AyelenIasich <ayeleniasich@gmail.com>
2022-08-16 14:48:32 -0300

    Se incorpora la opción de colocar un banner en el main, con su respectivo input en el editar.

--- src/app/models/persona.ts
+++ src/app/models/persona.ts
@@ -5,18 +5,21 @@ export class Persona {
   titulo?: string;
   descripcionMain?: string;
   imagenMain?: string;
+  banner?: string;
 
   constructor(
     nombre: string,
     apellido: string,
     titulo: string,
     descripcionMain: string,
-    imagenMain: string
+    imagenMain: string,
+    banner?: string
   ) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.titulo = titulo;
     this.descripcionMain = descripcionMain;
     this.imagenMain = imagenMain;
+    this.banner = banner;
   }
 }
